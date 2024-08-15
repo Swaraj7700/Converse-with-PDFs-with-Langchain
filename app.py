d7e11518d2c8bc21e1882663701c3ce870feaf64
Swaraj Phand <swarajphand@gmail.com>
2024-08-15 20:31:57 +0530

    push files

---
+++ app.py
@@ -0,0 +1,276 @@
+import streamlit as st
+from dotenv import load_dotenv
+import speech_recognition as sr
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template 
+from gtts import gTTS
+import os
+from PIL import Image
+from transformers import BlipProcessor, BlipForConditionalGeneration, DetrImageProcessor, DetrForObjectDetection
+import torch
+
+def speech_to_text_eng():
+    r = sr.Recognizer()
+    with sr.Microphone(device_index=1) as source:
+        st.write("Say something...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        return text
+    except sr.UnknownValueError:
+        st.write("Sorry, I could not understand what you said.")
+        return ""
+    except sr.RequestError as e:
+        st.write(f"Could not request results from Google Speech Recognition service; {e}")
+        return ""
+
+def speech_to_text_hindi(language='hi-IN'):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("कुछ कहिए...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio, language=language)
+        return text
+    except sr.UnknownValueError:
+        st.write("माफ़ कीजिए, मुझे आपके बोले शब्दों को समझ नहीं आया।")
+        return ""
+    except sr.RequestError as e:
+        st.write(f"Google वाणी संज्ञान सेवा से परिणाम अनुरोध नहीं किया जा सकता; {e}")
+        return ""
+    
+def speech_to_text_german(language='de-DE'):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Sprechen Sie etwas...")  # Update this line to display instructions in German
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio, language=language)
+        return text
+    except sr.UnknownValueError:
+        st.write("Entschuldigung, ich konnte Ihre gesprochenen Worte nicht verstehen.")
+        return ""
+    except sr.RequestError as e:
+        st.write(f"Die Google Spracherkennungsdienste konnten nicht angefordert werden; {e}")
+        return ""
+
+def speech_to_text_marathi(language='mr-IN'):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("आपल्यास कशी मदत करू ?")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio, language=language)
+        return text
+    except sr.UnknownValueError:
+        st.write("माफ़ करा, मला तुमच्या बोललेल्या शब्दांची समज नाही.")
+        return ""
+    except sr.RequestError as e:
+        st.write(f"Google वॉयस सुनवाई सेवेतून परिणाम विनंती केला जाऊ शकत नाही; {e}")
+        return ""
+    
+
+def get_image_caption(image_path):
+    """
+    Generates a short caption for the provided image.
+    Args:
+        image_path (str): The path to the image file.
+    Returns:
+        str: A string representing the caption for the image.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    model_name = "Salesforce/blip-image-captioning-large"
+    device = "cpu"  # cuda
+
+    processor = BlipProcessor.from_pretrained(model_name)
+    model = BlipForConditionalGeneration.from_pretrained(model_name).to(device)
+
+    inputs = processor(image, return_tensors='pt').to(device)
+    output = model.generate(**inputs, max_new_tokens=20)
+
+    caption = processor.decode(output[0], skip_special_tokens=True)
+
+    return caption
+
+
+def detect_objects(image_path):
+    """
+    Detects objects in the provided image.
+
+    Args:
+        image_path (str): The path to the image file.
+
+    Returns:
+        str: A string with all the detected objects. Each object as '[x1, x2, y1, y2, class_name, confindence_score]'.
+    """
+    image = Image.open(image_path).convert('RGB')
+
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = model(**inputs)
+
+    # convert outputs (bounding boxes and class logits) to COCO API
+    # let's only keep detections with score > 0.9
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+    detections = ""
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        detections += '[{}, {}, {}, {}]'.format(int(box[0]), int(box[1]), int(box[2]), int(box[3]))
+        detections += ' {}'.format(model.config.id2label[int(label)])
+        detections += ' {}\n'.format(float(score))
+
+    return detections
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def get_vectorstore(text_chunks):
+    embeddings = OpenAIEmbeddings()
+    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def get_conversation_chain(vectorstore):
+    llm = ChatOpenAI()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def text_to_speech(text, language='en'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save("output.mp3")
+
+    os.system("start output.mp3")
+
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+            text_to_speech(message.content)
+            
+
+
+
+def main():
+    load_dotenv()
+    st.set_page_config(page_title="Document Intelligence")
+    st.write(css, unsafe_allow_html=True)
+
+    option = st.sidebar.selectbox("Navigation", ["Document", "Image", "Speech"])
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    if option == "Document":
+        st.header("What does the document tell? :coffee:")
+        user_question = st.text_input("What do you wanna know?")
+        if user_question:
+            handle_userinput(user_question)
+
+        with st.sidebar:
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            if st.button("Process"):
+                with st.spinner("Processing"):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vectorstore = get_vectorstore(text_chunks)
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+    elif option == "Image":
+        st.header("Image Processing")
+        uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+
+        if uploaded_image is not None:
+            st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+            if st.button("Process"):
+                with st.spinner("Processing..."):
+                    # Save the uploaded image to a temporary location
+                    image_path = "temp_image.jpg"
+                    with open(image_path, "wb") as f:
+                        f.write(uploaded_image.read())
+
+                    # Detect objects
+                    detections = detect_objects(image_path)
+                    st.subheader("Detected Objects")
+                    st.text(detections)
+
+                    # Generate image caption
+                    caption = get_image_caption(image_path)
+                    st.subheader("Image Caption")
+                    st.write(caption)
+                    text_to_speech(caption)
+
+
+    elif option == "Speech":
+        st.header("Ask a question using speech")
+
+        lang_op = st.sidebar.selectbox("Language", ["English", "Hindi", "Marathi", "German"])
+
+        if st.button("Click to Speak"):
+            if(lang_op == "English"):
+                handle_userinput(speech_to_text_eng())
+            elif(lang_op == "Hindi"):
+                handle_userinput(speech_to_text_hindi())
+            elif(lang_op == "Marathi"):
+                handle_userinput(speech_to_text_marathi())
+            elif(lang_op == "German"):
+                handle_userinput(speech_to_text_german())
+        with st.sidebar:
+            st.subheader("Your documents")
+            pdf_docs = st.file_uploader(
+                "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+            if st.button("Process"):
+                with st.spinner("Processing"):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    vectorstore = get_vectorstore(text_chunks)
+                    st.session_state.conversation = get_conversation_chain(vectorstore)
+
+if __name__ == '__main__':
+    main()
